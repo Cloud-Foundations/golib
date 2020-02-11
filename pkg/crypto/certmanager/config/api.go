b94323285efbdbefdd00cfb5e57b825771c55116
Richard Gooch <rg+github0@safe-mbox.com>
2020-02-10 21:01:27 -0800

    Check in initial pkg/crypto/certmanager/config package.

---
+++ pkg/crypto/certmanager/config/api.go
@@ -0,0 +1,24 @@
+/*
+Package config wraps the certmanager and associated plugin packages and creates
+a certificate manager based on configuration data.
+*/
+package config
+
+import (
+	"github.com/Cloud-Foundations/golib/pkg/crypto/certmanager"
+	"github.com/Cloud-Foundations/golib/pkg/log"
+)
+
+type AcmeConfig struct {
+	ChallengeType       string   `yaml:"challenge_type"`
+	DomainNames         []string `yaml:"domain_names"`
+	HttpPort            uint16   `yaml:"http_port"`              // For http-01.
+	Route53HostedZoneId string   `yaml:"route53_hosted_zone_id"` // For dns-01.
+}
+
+func New(certFilename, keyFilename string, httpRedirectPort uint16,
+	config AcmeConfig,
+	logger log.DebugLogger) (*certmanager.CertificateManager, error) {
+	return newManager(certFilename, keyFilename, httpRedirectPort, config,
+		logger)
+}
