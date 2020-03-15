9e52b7414f2f29c212a771fd3bd3c3233e86bb82
Richard Gooch <rg+github0@safe-mbox.com>
2020-03-15 10:46:05 -0700

    certmanager storer: add unittests.

--- pkg/crypto/certmanager/storage/awssecretsmanager/impl.go
+++ pkg/crypto/certmanager/storage/awssecretsmanager/impl.go
@@ -26,6 +26,103 @@ var (
 	awsSecretsManagerMetadataClientError error
 )
 
+func decodeCert(encodedCert string) (*certmanager.Certificate, error) {
+	var keyMap map[string]string
+	if err := json.Unmarshal([]byte(encodedCert), &keyMap); err != nil {
+		return nil, fmt.Errorf("error unmarshaling secret: %s", err)
+	}
+	certPEM := &bytes.Buffer{}
+	for index := 0; ; index++ {
+		certificateBase64 := keyMap[fmt.Sprintf("Certificate%d", index)]
+		if certificateBase64 == "" {
+			if index == 0 {
+				return nil, errors.New("no Certificate in map")
+			}
+			break // We've reached the end of the certificate chain.
+		}
+		certDER, err := base64.StdEncoding.DecodeString(
+			strings.Replace(certificateBase64, " ", "", -1))
+		if err != nil {
+			return nil, err
+		}
+		if index != 0 {
+			fmt.Fprintln(certPEM)
+		}
+		err = pem.Encode(certPEM, &pem.Block{
+			Type:  "CERTIFICATE",
+			Bytes: certDER,
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+	keyType := keyMap["KeyType"]
+	if keyType != "" {
+		keyType += " "
+	}
+	privateKeyBase64 := keyMap["PrivateKey"]
+	if privateKeyBase64 == "" {
+		return nil, errors.New("no PrivateKey in map")
+	}
+	privateKey, err := base64.StdEncoding.DecodeString(
+		strings.Replace(privateKeyBase64, " ", "", -1))
+	if err != nil {
+		return nil, err
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  keyType + "PRIVATE KEY",
+		Bytes: privateKey,
+	})
+	return &certmanager.Certificate{
+		CertPemBlock: certPEM.Bytes(),
+		KeyPemBlock:  keyPEM,
+	}, nil
+}
+
+func encodeCert(cert *certmanager.Certificate) (string, error) {
+	keyMap := make(map[string]string, 4)
+	// Decode all the certificates in the chain.
+	next := cert.CertPemBlock
+	for index := 0; ; index++ {
+		var certBlock *pem.Block
+		certBlock, next = pem.Decode(next)
+		if certBlock == nil {
+			if index == 0 {
+				return "", errors.New("unable to decode any PEM Certificate")
+			}
+			break // We've reached the end of the certificate chain.
+		}
+		if certBlock.Type != "CERTIFICATE" {
+			return "", fmt.Errorf("Certificate type: %s not supported",
+				certBlock.Type)
+		}
+		keyMap[fmt.Sprintf("Certificate%d", index)] =
+			base64.StdEncoding.EncodeToString(certBlock.Bytes)
+	}
+	// Decode the private key.
+	keyBlock, _ := pem.Decode(cert.KeyPemBlock)
+	if keyBlock == nil {
+		return "", errors.New("unable to decode PEM PrivateKey")
+	}
+	if keyBlock.Type != "PRIVATE KEY" {
+		splitKeyType := strings.SplitN(keyBlock.Type, " ", 2)
+		if len(splitKeyType) != 2 {
+			return "", fmt.Errorf("unable to split: %s", keyBlock.Type)
+		}
+		if splitKeyType[1] != "PRIVATE KEY" {
+			return "", fmt.Errorf("PrivateKey type: %s not supported",
+				keyBlock.Type)
+		}
+		keyMap["KeyType"] = splitKeyType[0]
+	}
+	keyMap["PrivateKey"] = base64.StdEncoding.EncodeToString(keyBlock.Bytes)
+	encodedCert, err := json.Marshal(keyMap)
+	if err != nil {
+		return "", err
+	}
+	return string(encodedCert), nil
+}
+
 func getMetadataClient() (*ec2metadata.EC2Metadata, error) {
 	awsSecretsManagerLock.Lock()
 	defer awsSecretsManagerLock.Unlock()
@@ -72,22 +169,17 @@ func getAwsService(secretId string) (*secretsmanager.SecretsManager, error) {
 }
 
 func getAwsSecret(awsService *secretsmanager.SecretsManager,
-	secretId string) (map[string]string, error) {
+	secretId string) (string, error) {
 	input := secretsmanager.GetSecretValueInput{SecretId: aws.String(secretId)}
 	output, err := awsService.GetSecretValue(&input)
 	if err != nil {
-		return nil,
+		return "",
 			fmt.Errorf("error calling secretsmanager:GetSecretValue: %s", err)
 	}
 	if output.SecretString == nil {
-		return nil, errors.New("no SecretString in secret")
+		return "", errors.New("no SecretString in secret")
 	}
-	secret := []byte(*output.SecretString)
-	var secrets map[string]string
-	if err := json.Unmarshal(secret, &secrets); err != nil {
-		return nil, fmt.Errorf("error unmarshaling secret: %s", err)
-	}
-	return secrets, nil
+	return *output.SecretString, nil
 }
 
 func newLS(secretId string, logger log.DebugLogger) (*LockingStorer, error) {
@@ -103,14 +195,10 @@ func newLS(secretId string, logger log.DebugLogger) (*LockingStorer, error) {
 }
 
 func putAwsSecret(awsService *secretsmanager.SecretsManager,
-	secretId string, secrets map[string]string) error {
-	secretString, err := json.Marshal(secrets)
-	if err != nil {
-		return err
-	}
+	secretId string, secretString string) error {
 	input := secretsmanager.PutSecretValueInput{
 		SecretId:     aws.String(secretId),
-		SecretString: aws.String(string(secretString)),
+		SecretString: aws.String(secretString),
 	}
 	output, err := awsService.PutSecretValue(&input)
 	if err != nil {
@@ -126,96 +214,26 @@ func putAwsSecret(awsService *secretsmanager.SecretsManager,
 }
 
 func (ls *LockingStorer) read() (*certmanager.Certificate, error) {
-	keyMap, err := getAwsSecret(ls.awsService, ls.secretId)
+	secret, err := getAwsSecret(ls.awsService, ls.secretId)
 	if err != nil {
 		return nil, err
 	}
-	certPEM := &bytes.Buffer{}
-	for index := 0; ; index++ {
-		certificateBase64 := keyMap[fmt.Sprintf("Certificate%d", index)]
-		if certificateBase64 == "" {
-			if index == 0 {
-				return nil, errors.New("no Certificate in map")
-			}
-			break // We've reached the end of the certificate chain.
-		}
-		certDER, err := base64.StdEncoding.DecodeString(
-			strings.Replace(certificateBase64, " ", "", -1))
-		if err != nil {
-			return nil, err
-		}
-		if index != 0 {
-			fmt.Fprintln(certPEM)
-		}
-		err = pem.Encode(certPEM, &pem.Block{
-			Type:  "CERTIFICATE",
-			Bytes: certDER,
-		})
-		if err != nil {
-			return nil, err
-		}
-	}
-	keyType := keyMap["KeyType"]
-	if keyType == "" {
-		return nil, errors.New("no KeyType in map")
-	}
-	privateKeyBase64 := keyMap["PrivateKey"]
-	if privateKeyBase64 == "" {
-		return nil, errors.New("no PrivateKey in map")
-	}
-	privateKey, err := base64.StdEncoding.DecodeString(
-		strings.Replace(privateKeyBase64, " ", "", -1))
+	cert, err := decodeCert(secret)
 	if err != nil {
 		return nil, err
 	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{
-		Type:  keyType + " PRIVATE KEY",
-		Bytes: privateKey,
-	})
 	ls.logger.Printf(
 		"read certificate from AWS Secrets Manager, SecretId: %s\n",
 		ls.secretId)
-	return &certmanager.Certificate{
-		CertPemBlock: certPEM.Bytes(),
-		KeyPemBlock:  keyPEM,
-	}, nil
+	return cert, nil
 }
 
 func (ls *LockingStorer) write(cert *certmanager.Certificate) error {
-	keyMap := make(map[string]string, 4)
-	// Decode all the certificates in the chain.
-	next := cert.CertPemBlock
-	for index := 0; ; index++ {
-		var certBlock *pem.Block
-		certBlock, next = pem.Decode(next)
-		if certBlock == nil {
-			if index == 0 {
-				return errors.New("unable to decode any PEM Certificate")
-			}
-			break // We've reached the end of the certificate chain.
-		}
-		if certBlock.Type != "CERTIFICATE" {
-			return fmt.Errorf("Certificate type: %s not supported",
-				certBlock.Type)
-		}
-		keyMap[fmt.Sprintf("Certificate%d", index)] =
-			base64.StdEncoding.EncodeToString(certBlock.Bytes)
+	secret, err := encodeCert(cert)
+	if err != nil {
+		return err
 	}
-	// Decode the private key.
-	keyBlock, _ := pem.Decode(cert.KeyPemBlock)
-	if keyBlock == nil {
-		return errors.New("unable to decode PEM PrivateKey")
-	}
-	splitKeyType := strings.SplitN(keyBlock.Type, " ", 2)
-	if len(splitKeyType) != 2 {
-		return fmt.Errorf("unable to split: %s", keyBlock.Type)
-	}
-	if splitKeyType[1] != "PRIVATE KEY" {
-		return fmt.Errorf("PrivateKey type: %s not supported", keyBlock.Type)
-	}
-	keyMap["KeyType"] = splitKeyType[0]
-	keyMap["PrivateKey"] = base64.StdEncoding.EncodeToString(keyBlock.Bytes)
-	if err := putAwsSecret(ls.awsService, ls.secretId, keyMap); err != nil {
+	if err := putAwsSecret(ls.awsService, ls.secretId, secret); err != nil {
 		return err
 	}
 	ls.logger.Printf("wrote certificate to AWS Secrets Manager, SecretId: %s\n",
