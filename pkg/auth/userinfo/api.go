1cba9412f9abccf2c010397f5d818c4e5f7b34ba
Richard Gooch <rg+github0@safe-mbox.com>
2020-04-09 17:34:45 -0700

    Add pkg/auth/userinfo.UsersInGroupGetter interface.

--- pkg/auth/userinfo/api.go
+++ pkg/auth/userinfo/api.go
@@ -8,6 +8,13 @@ type UserGroupsGetter interface {
 	GetUserGroups(username string) ([]string, error)
 }
 
+// UsersInGroupGetter is the interface that wraps the GetUsersInGroup method.
+//
+// GetUsersInGroup gets the list of users which are members of the group.
+type UsersInGroupGetter interface {
+	GetUsersInGroup(groupname string) ([]string, error)
+}
+
 // UserInfo is the interface that wraps multiple methods which yield information
 // about a user.
 type UserInfo interface {
