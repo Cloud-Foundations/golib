474676c7a66a08867560d9e8a977eca79b14e2a0
Richard Gooch <rg+github0@safe-mbox.com>
2020-04-28 08:07:25 -0700

    acme-proxy: add -fallbackPortNum option.

--- cmd/acme-proxy/main.go
+++ cmd/acme-proxy/main.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/Cloud-Foundations/Dominator/lib/flags/loadflags"
@@ -20,6 +19,8 @@ const acmePath = "/.well-known/acme-challenge"
 var (
 	acmePortNum = flag.Uint("acmePortNum", 80,
 		"Port number to allocate and listen on for ACME http-01 challenges")
+	fallbackPortNum = flag.Uint("fallbackPortNum", 0,
+		"Backend port number to connect to if port 80 yields 404: Not Found")
 )
 
 type acmeProxy struct {
@@ -41,8 +42,7 @@ func doMain() error {
 	flag.Parse()
 	tricorder.RegisterFlags()
 	server := &acmeProxy{serverlogger.New("")}
-	return http.ListenAndServe(":"+strconv.FormatUint(uint64(*acmePortNum), 10),
-		server)
+	return http.ListenAndServe(fmt.Sprintf(":%d", *acmePortNum), server)
 }
 
 func main() {
@@ -72,6 +72,23 @@ func (proxy *acmeProxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusNotFound && *fallbackPortNum != 0 {
+		newUrl = fmt.Sprintf("http://%s:%d%s",
+			req.Host, *fallbackPortNum, req.URL.Path)
+		resp, err = http.DefaultClient.Get(newUrl)
+		if err != nil {
+			proxy.logger.Println(err)
+			http.Error(w, "Error getting response",
+				http.StatusServiceUnavailable)
+			return
+		}
+		defer resp.Body.Close()
+	}
+	if resp.StatusCode == http.StatusOK {
+		proxy.logger.Printf("%s: OK\n", newUrl)
+	} else {
+		proxy.logger.Printf("%s: %s\n", newUrl, resp.Status)
+	}
 	w.WriteHeader(resp.StatusCode)
 	if _, err := io.Copy(w, resp.Body); err != nil {
 		proxy.logger.Printf("%s: error copying body: %s\n", newUrl, err)
