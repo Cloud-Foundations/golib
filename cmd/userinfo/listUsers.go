46bb4c17d6c6163a3a79285b2cff346adaf44549
Richard Gooch <rg+github0@safe-mbox.com>
2021-02-02 08:03:21 -0800

    userinfo: use new getDB() in list-users subcommand.

--- cmd/userinfo/listUsers.go
+++ cmd/userinfo/listUsers.go
@@ -3,13 +3,10 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 	"sort"
-	"time"
 
 	"github.com/Cloud-Foundations/Dominator/lib/log"
-	"github.com/Cloud-Foundations/golib/pkg/auth/userinfo/gitdb"
 )
 
 func listUsersSubcommand(args []string, logger log.DebugLogger) error {
@@ -20,19 +17,7 @@ func listUsersSubcommand(args []string, logger log.DebugLogger) error {
 }
 
 func listUsers(writer io.Writer, source string, logger log.DebugLogger) error {
-	var tmpdir string
-	fi, err := os.Stat(source)
-	if err == nil && fi.IsDir() {
-		tmpdir = source
-		source = ""
-	} else {
-		tmpdir, err = ioutil.TempDir("", "userinfo")
-		if err != nil {
-			return err
-		}
-		defer os.RemoveAll(tmpdir)
-	}
-	db, err := gitdb.New(source, "", tmpdir, time.Hour, logger)
+	db, err := getDB(source, logger)
 	if err != nil {
 		return err
 	}
