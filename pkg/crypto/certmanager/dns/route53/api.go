905265c3e5cd95b1a2d19943a38de8ffaf958de1
Richard Gooch <rg+github0@safe-mbox.com>
2021-04-14 08:18:27 -0700

    Move pkg/crypto/certmanager/dns/route53 generic code.

--- pkg/crypto/certmanager/dns/route53/api.go
+++ pkg/crypto/certmanager/dns/route53/api.go
@@ -4,34 +4,13 @@ Package route53 implements a dns-01 ACME protocol responder using AWS Route 53.
 package route53
 
 import (
-	"time"
-
+	"github.com/Cloud-Foundations/golib/pkg/crypto/certmanager"
 	"github.com/Cloud-Foundations/golib/pkg/log"
 )
-
-type recordDeleteWriter interface {
-	DeleteRecords(fqdn, recType string) error
-	WriteRecords(fqdn, recType string, recs []string, ttl time.Duration) error
-}
-
-type Responder struct {
-	rdw    recordDeleteWriter
-	logger log.DebugLogger
-	// Mutable data follow.
-	records map[string]string
-}
 
 // New creates a DNS responder for ACME dns-01 challenges.
 // The logger is used for logging messages.
 func New(hostedZoneId string,
-	logger log.DebugLogger) (*Responder, error) {
+	logger log.DebugLogger) (certmanager.Responder, error) {
 	return newResponder(hostedZoneId, logger)
-}
-
-func (r *Responder) Cleanup() {
-	r.cleanup()
-}
-
-func (r *Responder) Respond(key, value string) error {
-	return r.respond(key, value)
 }
--- pkg/crypto/certmanager/dns/route53/impl.go
+++ pkg/crypto/certmanager/dns/route53/impl.go
@@ -2,8 +2,8 @@ package route53
 
 import (
 	"errors"
-	"time"
 
+	"github.com/Cloud-Foundations/golib/pkg/crypto/certmanager"
 	"github.com/Cloud-Foundations/golib/pkg/dns/route53"
 	"github.com/Cloud-Foundations/golib/pkg/log"
 	"github.com/aws/aws-sdk-go/aws"
@@ -11,7 +11,7 @@ import (
 )
 
 func newResponder(hostedZoneId string,
-	logger log.DebugLogger) (*Responder, error) {
+	logger log.DebugLogger) (certmanager.Responder, error) {
 	if hostedZoneId == "" {
 		return nil, errors.New("no hosted zone ID specified")
 	}
@@ -26,35 +26,5 @@ func newResponder(hostedZoneId string,
 	if err != nil {
 		return nil, err
 	}
-	return &Responder{
-		rdw:     rdw,
-		logger:  logger,
-		records: make(map[string]string),
-	}, nil
-}
-
-func (r *Responder) cleanup() {
-	if len(r.records) < 1 {
-		return
-	}
-	for fqdn := range r.records {
-		if err := r.rdw.DeleteRecords(fqdn, "TXT"); err != nil {
-			r.logger.Println(err)
-		} else {
-			delete(r.records, fqdn)
-		}
-	}
-}
-
-func (r *Responder) respond(key, value string) error {
-	if r.records[key] == value {
-		return nil
-	}
-	r.logger.Debugf(1, "publishing %s TXT=\"%s\"\n", key, value)
-	err := r.rdw.WriteRecords(key, "TXT", []string{value}, time.Second*15)
-	if err != nil {
-		return err
-	}
-	r.records[key] = value
-	return nil
+	return certmanager.MakeDnsResponder(rdw, logger)
 }
