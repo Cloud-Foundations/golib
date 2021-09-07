f66a8c794645da57138030a7b18fca407013c9b1
Richard Gooch <rg+github0@safe-mbox.com>
2021-09-07 06:02:00 -0700

    pkg/constants: add OIDs.

--- pkg/constants/api.go
+++ pkg/constants/api.go
@@ -7,4 +7,9 @@ const (
 	AcmePath                  = "/.well-known/acme-challenge"
 	AcmeProxyCleanupResponses = "/api/responses/cleanup"
 	AcmeProxyRecordResponse   = "/api/responses/recordOne"
+
+	// Copied from github.com/Cloud-Foundations/Dominator/constants
+	AssignedOIDBase        = "1.3.6.1.4.1.9586.100.7"
+	PermittedMethodListOID = AssignedOIDBase + ".1"
+	GroupListOID           = AssignedOIDBase + ".2"
 )
