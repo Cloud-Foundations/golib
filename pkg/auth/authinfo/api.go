fc545b0034df0e57d38cc90ca0bf2c0318007ffe
Richard Gooch <rg+github0@safe-mbox.com>
2021-09-08 06:38:04 -0700

    pkg/auth/authinfo: initial checkin.

---
+++ pkg/auth/authinfo/api.go
@@ -0,0 +1,33 @@
+package authinfo
+
+import (
+	"sync"
+)
+
+// AuthInfo contains authentication information.
+type AuthInfo struct {
+	Groups           []string
+	PermittedMethods []string
+	Username         string
+	mutex            sync.Mutex // Protect everything below.
+	groups           map[string]struct{}
+	permittedMethods map[string]struct{}
+}
+
+// ListToMap is a convenience function to convert a slice of strings to a map
+// of strings.
+func ListToMap(list []string) map[string]struct{} {
+	return listToMap(list)
+}
+
+// MapToList is a convenience function to convert a map of strings to a sorted
+// slice of strings.
+func MapToList(list map[string]struct{}) []string {
+	return mapToList(list)
+}
+
+// CheckGroup will return true if the specified group is present in the list.
+// It uses an O(1) lookup.
+func (ai *AuthInfo) CheckGroup(group string) bool {
+	return ai.checkGroup(group)
+}
