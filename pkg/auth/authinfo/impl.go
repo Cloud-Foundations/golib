26385be3e5db72975628fb41f9d67bf7df09144b
Richard Gooch <rg+github0@safe-mbox.com>
2021-09-22 07:43:45 -0700

    pkg/auth/authinfo: add AuthInfo.Write() method.

--- pkg/auth/authinfo/impl.go
+++ pkg/auth/authinfo/impl.go
@@ -2,6 +2,8 @@ package authinfo
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"sort"
 )
 
@@ -40,6 +42,13 @@ func mapToList(list map[string]struct{}) []string {
 	return sortedList
 }
 
+func writeList(writer io.Writer, list []string, prefix, space, postfix string) {
+	for _, entry := range list {
+		fmt.Fprintf(writer, "%s%s%s%s%s\n",
+			prefix, space, space, entry, postfix)
+	}
+}
+
 func (ai *AuthInfo) checkGroup(group string) bool {
 	ai.mutex.Lock()
 	defer ai.mutex.Unlock()
@@ -48,4 +57,37 @@ func (ai *AuthInfo) checkGroup(group string) bool {
 	}
 	_, ok := ai.groups[group]
 	return ok
+}
+
+func (ai *AuthInfo) write(writer io.Writer,
+	prefix, space, postfix string) error {
+	if ai == nil {
+		fmt.Fprintf(writer, "%sNo authentication information%s\n",
+			prefix, postfix)
+	} else {
+		if ai.Username != "" {
+			fmt.Fprintf(writer, "%sUsername: %s%s\n",
+				prefix, ai.Username, postfix)
+		} else if ai.AwsRole != nil {
+			fmt.Fprintf(writer, "%sAWS role: %s in account: %s (ARN=%s)%s\n",
+				prefix, ai.AwsRole.Name, ai.AwsRole.AccountId,
+				ai.AwsRole.ARN, postfix)
+		} else {
+			fmt.Fprintf(writer, "%sUnknown principal%s\n", prefix, postfix)
+		}
+		if len(ai.Groups) > 0 {
+			fmt.Fprintf(writer, "%sGroup list:%s\n", prefix, postfix)
+			writeList(writer, ai.Groups, prefix, space, postfix)
+		} else {
+			fmt.Fprintf(writer, "%sNo group memberships%s\n", prefix, postfix)
+		}
+		if len(ai.PermittedMethods) > 0 {
+			fmt.Fprintf(writer, "%sPermitted methods:%s\n", prefix, postfix)
+			writeList(writer, ai.PermittedMethods, prefix, space, postfix)
+		} else {
+			fmt.Fprintf(writer, "%sNo methods are permitted%s\n",
+				prefix, postfix)
+		}
+	}
+	return nil
 }
