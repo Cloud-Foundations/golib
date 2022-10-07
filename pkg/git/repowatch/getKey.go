d1a7c34a0ddef726975d8ad46124ebe09d21dfc4
Richard Gooch <rg+github0@safe-mbox.com>
2022-10-07 06:06:14 -0700

    pkg/git/repowatch: fix when not using SSH agent.

--- pkg/git/repowatch/getKey.go
+++ pkg/git/repowatch/getKey.go
@@ -13,26 +13,47 @@ import (
 	"github.com/Cloud-Foundations/golib/pkg/log"
 
 	"github.com/Cloud-Foundations/Dominator/lib/fsutil"
+	"github.com/go-git/go-git/v5/plumbing/transport"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
+	xssh "golang.org/x/crypto/ssh"
 )
 
-func awsGetKey(secretId string, logger log.DebugLogger) error {
+func awsGetKey(secretId string, logger log.DebugLogger) (
+	transport.AuthMethod, error) {
 	if secretId == "" {
-		return nil
+		if os.Getenv("SSH_AUTH_SOCK") == "" {
+			logger.Debugln(0, "using default SSH configuration")
+			return nil, nil // Hope for the best.
+		}
+		if pkc, err := ssh.NewSSHAgentAuth(ssh.DefaultUsername); err != nil {
+			return nil, err
+		} else {
+			pkc.HostKeyCallbackHelper.HostKeyCallback =
+				xssh.InsecureIgnoreHostKey()
+			return pkc, nil
+		}
 	}
 	metadataClient, err := getMetadataClient()
 	if err != nil {
-		return err
+		return nil, err
 	}
 	secrets, err := getAwsSecret(metadataClient, secretId)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if err := writeSshKey(secrets); err != nil {
-		return err
+	filename, err := writeSshKey(secrets)
+	if err != nil {
+		return nil, err
 	}
-	logger.Printf("fetched SSH key from AWS Secrets Manager, SecretId: %s\n",
-		secretId)
-	return nil
+	logger.Debugf(0,
+		"fetched SSH key from AWS Secrets Manager, SecretId: %s and wrote to: %s\n",
+		secretId, filename)
+	pubkeys, err := ssh.NewPublicKeysFromFile(ssh.DefaultUsername, filename, "")
+	if err != nil {
+		return nil, err
+	}
+	pubkeys.HostKeyCallbackHelper.HostKeyCallback = xssh.InsecureIgnoreHostKey()
+	return pubkeys, nil
 }
 
 // keyMap is mutated.
@@ -61,10 +82,10 @@ func writeKeyAsPEM(writer io.Writer, keyMap map[string]string) error {
 }
 
 // keyMap is mutated.
-func writeSshKey(keyMap map[string]string) error {
+func writeSshKey(keyMap map[string]string) (string, error) {
 	dirname := filepath.Join(os.Getenv("HOME"), ".ssh")
 	if err := os.MkdirAll(dirname, 0700); err != nil {
-		return err
+		return "", err
 	}
 	var filename string
 	switch keyType := keyMap["KeyType"]; keyType {
@@ -73,16 +94,16 @@ func writeSshKey(keyMap map[string]string) error {
 	case "RSA":
 		filename = "id_rsa"
 	default:
-		return fmt.Errorf("unsupported key type: %s", keyType)
+		return "", fmt.Errorf("unsupported key type: %s", keyType)
 	}
 	writer, err := fsutil.CreateRenamingWriter(filepath.Join(dirname, filename),
 		fsutil.PrivateFilePerms)
 	if err != nil {
-		return err
+		return "", err
 	}
 	if err := writeKeyAsPEM(writer, keyMap); err != nil {
 		writer.Abort()
-		return err
+		return "", err
 	}
-	return writer.Close()
+	return filename, writer.Close()
 }
